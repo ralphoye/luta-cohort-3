e118e041b24a0258b3939b544f797fe4a1457442
ralphoye <oyeniyi.raphael@lmu.edu.ng>
2023-08-01 18:42:23 +0100

    FEM Project challenge

---
+++ UBA Landing Page/index.js
@@ -0,0 +1,10 @@
+
+
+const myElement = document.getElementById("demo");
+myElement.style.color = "red";
+
+let panel = document.getElementsByClassName('panel');
+fix.setAttribute('style','background-colour:blue');
+
+// let demo = [5]
+// console.log(demo);
